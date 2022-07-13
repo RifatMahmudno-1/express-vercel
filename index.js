cb4de07f67197e17ce4501eafc905f64cfea1111
Rifat Mahmud <rifatmahmudpc@gmail.com>
2022-07-13 14:41:15 +0600

    test

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.json({ extended: false }))
 
+let upTime = 0
+
+setInterval(() => {
+	upTime += 1
+}, 1000)
+
 app.get('/', (req, res) => {
-	res.send({ Success: true })
+	res.send({ upTime })
 })
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
